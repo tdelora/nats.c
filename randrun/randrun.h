65feea4dd7bbc1993abd5d84cc3dcb4accdf11ea
Thomas De Lora <TED@Sardi.local>
2022-05-03 11:44:18 -0700

    Created rr_nouns_verbs.h

--- randrun/randrun.h
+++ randrun/randrun.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "rr_nouns_verbs.h"
 #include "rr_random.h"
 
 
---
+++ randrun/rr_nouns_verbs.h
@@ -0,0 +1,8 @@
+#ifndef RR_NOUNS_VERBS_
+#define RR_NOUNS_VERBS_
+
+#include "rr_random.h"
+
+typedef enum { CONNECTION, PUBLISHER, SUBSCRIBER } rr_nats_nouns;
+
+#endif /* RR_NOUNS_VERBS_ */
