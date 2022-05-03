468dcf8359d64b0589fb4884328c4f148910d348
Thomas De Lora <TED@Sardi.local>
2022-05-03 15:48:42 -0700

    Fixed edit mistake in rr_nouns_verbs.c

--- randrun/rr_nouns_verbs.c
+++ randrun/rr_nouns_verbs.c
@@ -1,7 +1,7 @@
 #include "rr_nouns_verbs.h"
 
 rr_nats_nouns rr_get_randon_nats_noun(void) {
-    int nounRand = rr_rand_lim(RR_NATS_NOUN_COUNT); 
+    int nounRand = rr_rand_limit(RR_NATS_NOUN_COUNT); 
     rr_nats_nouns returnValue = (rr_nats_nouns) nounRand;
 
     return(returnValue);
