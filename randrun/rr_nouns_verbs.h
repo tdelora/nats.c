248afee48d9d6bba880f0f0837b05e10669fcc94
Thomas De Lora <TED@Sardi.local>
2022-05-03 15:31:31 -0700

    Minor changes to rr_nouns_verbs.h

--- randrun/rr_nouns_verbs.h
+++ randrun/rr_nouns_verbs.h
@@ -4,10 +4,7 @@
 #include "rr_random.h"
 
 typedef enum { NATS_CONNECTION = 0, NATS_PUBLISHER, NATS_SUBSCRIBER } rr_nats_nouns;
-#define RR_NATS_NOUN_COUNT 3
-/*
-#define RR_NATS_NOUN_COUNT (sizeof(rr_nats_nouns)/sizeof(rr_nats_nouns[0]))
-*/
+#define RR_NATS_NOUN_COUNT 3 /* Fixme - autocalculate size */
 
 rr_nats_nouns rr_get_randon_nats_noun(void);
 
