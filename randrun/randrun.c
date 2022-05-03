d526e844d19593345fb4ddcfa9ae67384a02bfe3
Thomas De Lora <TED@Sardi.local>
2022-05-03 15:14:22 -0700

    Added rr_nouns_verbs.c and added nats noun code.

--- randrun/randrun.c
+++ randrun/randrun.c
@@ -1,14 +1,38 @@
 #include "randrun.h"
 
 int main() {
-   int r = 0, count = 100;
+   int randomSeed = 0, count = 100, returnValue = 0;
 
-   rr_init_rand(0);
+   rr_nats_nouns natsNoun = NATS_CONNECTION;
+   
+   /* Initialize rand */
+   rr_init_rand(randomSeed);
 
-   while (count) {
-      r = rr_rand_lim(10);
-      printf("%d\n",r);
+   while (count && !returnValue) {
+      natsNoun = rr_get_randon_nats_noun();
+
+      switch (natsNoun)
+      {
+      case NATS_CONNECTION:
+         fprintf(stdout,"%s","NATS_CONNECTION");
+         break;
+      
+      case NATS_PUBLISHER:
+         fprintf(stdout,"%s","NATS_PUBLISHER");
+         break;
+
+      case NATS_SUBSCRIBER:
+         fprintf(stdout,"%s","NATS_SUBSCRIBER");
+         break;
+      
+      default:
+         fprintf(stderr,"Main received unexpected nats noun %d\n",natsNoun);
+         returnValue = 1;
+         break;
+      }
+
+      fprintf(stdout,"\n");
       count--;
    }
-   return(0);  
+   return(returnValue);  
 }
--- randrun/rr_nouns_verbs.h
+++ randrun/rr_nouns_verbs.h
@@ -3,6 +3,12 @@
 
 #include "rr_random.h"
 
-typedef enum { CONNECTION, PUBLISHER, SUBSCRIBER } rr_nats_nouns;
+typedef enum { NATS_CONNECTION = 0, NATS_PUBLISHER, NATS_SUBSCRIBER } rr_nats_nouns;
+#define RR_NATS_NOUN_COUNT 3
+/*
+#define RR_NATS_NOUN_COUNT (sizeof(rr_nats_nouns)/sizeof(rr_nats_nouns[0]))
+*/
+
+rr_nats_nouns rr_get_randon_nats_noun(void);
 
 #endif /* RR_NOUNS_VERBS_ */
