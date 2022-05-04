5af17e1c5244160b5fd72d7e79c9793c693d2fb1
Thomas De Lora <TED@Sardi.local>
2022-05-03 17:49:27 -0700

    Renamed rr_nouns_verbs.[ch] to rr_nats_nouns_verbs.[ch]

--- randrun/randrun.h
+++ randrun/randrun.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "rr_nouns_verbs.h"
+#include "rr_nats_nouns_verbs.h"
 #include "rr_random.h"
 
 
--- randrun/rr_nats_nouns_verbs.c
+++ randrun/rr_nats_nouns_verbs.c
@@ -1,4 +1,4 @@
-#include "rr_nouns_verbs.h"
+#include "rr_nats_nouns_verbs.h"
 
 rr_nats_nouns rr_get_randon_nats_noun(void) {
     int nounRand = rr_rand_lim(RR_NATS_NOUN_COUNT); 
