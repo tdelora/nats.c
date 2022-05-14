0b0583c293b01c7c7f02315c8fccf97fc5a197da
Thomas De Lora <TED@Sardi.local>
2022-05-13 20:45:15 -0700

    Safety checkin - DEFINITLY NOT READY FOR PRIME TIME

--- randrun/randrun.c
+++ randrun/randrun.c
@@ -1,64 +1,37 @@
 #include "randrun.h"
 
 int main(int argc, char ** argv) {
-   int count = 0, returnValue = 0;
-   char errorString[ERROR_STR_BUFFER_SIZE];
-
-   rr_nats_nouns natsNoun = NATS_CONNECTION;
+   int returnValue = 0;
    
    /* Initialize steps */
-   memset(errorString,0,ERROR_STR_BUFFER_SIZE);
    /* Init global values */
    randrunInit();
 
    /* Read Command Line */
    returnValue = parseArgs(argc,argv);
 
-   /* Initialize rand */
-   rr_init_rand(randomSeed);
+   if ( returnValue == 0 ) {
+      /* Initialize rand */
+      rr_init_rand(randomSeed);
 
-   if ( ! isatty(fileno(stdin)) ) {
-      /* File read */
-      fprintf(stdout,"\nFile read not implemented\n");
-      exit(1);
-   }
-   else {
-      while (count < maxSteps && !returnValue) {
-         natsNoun = rr_get_randon_nats_noun();
-
-         switch (natsNoun)
-         {
-            case NATS_CONNECTION:
-               rr_writeWorkLine((char *) "NATS_CONNECTION");
-               break;
-      
-            case NATS_PUBLISHER:
-               rr_writeWorkLine((char *) "NATS_PUBLISHER");
-               break;
-
-            case NATS_SUBSCRIBER:
-               rr_writeWorkLine((char *) "NATS_SUBSCRIBER");
-               break;
-      
-            default:
-               sprintf(errorString,"Main received unexpected nats noun %d\n",natsNoun);
-               rr_writeErrorLine(errorString);
-               returnValue = 1;
-               break;
-         }
-
-         fprintf(stdout,"\n");
-         count++;
+      if ( ! isatty(fileno(stdin)) ) {
+         /* File read */
+         returnValue = readFromFile();
+      }
+      else {
+         returnValue = generateRandomly();
       }
    }
-   
+
    return(returnValue);  
 } /* main() */
+
 
 void randrunInit(void) {
    randomSeed = 0;
    maxSteps = 100;
 } /* randrunInit() */
+
 
 int parseArgs(int argc, char ** argv) {
    int i = 0, returnValue = 0;
@@ -89,6 +62,7 @@ int parseArgs(int argc, char ** argv) {
    return(returnValue);
 } /* parseArgs() */
 
+
 void printUsageAndExit(char * progName, char * complain) {
 
    if ( complain!= NULL ) {
@@ -104,3 +78,69 @@ void printUsageAndExit(char * progName, char * complain) {
    ,progName);
    exit(1);
 } /* printUsageAndExit */
+
+
+int readFromFile(void) {
+   int returnValue = 0, sizeRead = 0;
+   char fileReadBuffer[FILE_READ_BUFFER_SIZE];
+
+   memset(fileReadBuffer,0,FILE_READ_BUFFER_SIZE);
+
+   /* Read from stdin and break up the results */
+   while ((sizeRead = read(fileno(stdin), fileReadBuffer, FILE_READ_BUFFER_SIZE)) > 0) {
+      fprintf(stdout,"%s",fileReadBuffer);
+
+      char* token = strtok(fileReadBuffer, "\n");
+      while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, "\n");
+    }
+
+      /* rr_writeWorkLine(fileReadBuffer); */
+
+      memset(fileReadBuffer,0,FILE_READ_BUFFER_SIZE);
+   }
+
+   return(returnValue);
+}  /* readFromFile */
+
+
+int generateRandomly(void) {
+   int count = 0, returnValue = 0;
+   char errorString[ERROR_STR_BUFFER_SIZE];
+   rr_nats_nouns natsNoun = NATS_CONNECTION;
+
+   memset(errorString,0,ERROR_STR_BUFFER_SIZE);
+
+   while (count < maxSteps && !returnValue) {
+
+      natsNoun = rr_get_randon_nats_noun();
+
+      switch (natsNoun)
+      {
+         case NATS_CONNECTION:
+            rr_writeWorkLine((char *) "NATS_CONNECTION");
+            break;
+      
+         case NATS_PUBLISHER:
+            rr_writeWorkLine((char *) "NATS_PUBLISHER");
+            break;
+
+         case NATS_SUBSCRIBER:
+            rr_writeWorkLine((char *) "NATS_SUBSCRIBER");
+            break;
+      
+         default:
+            sprintf(errorString,"Main received unexpected nats noun %d\n",natsNoun);
+            rr_writeErrorLine(errorString);
+            returnValue = 1;
+            break;
+      }
+
+      fprintf(stdout,"\n");
+      count++;
+   }
+
+   return(returnValue);
+} /* generateRandomly */
+
--- randrun/randrun.h
+++ randrun/randrun.h
@@ -12,10 +12,14 @@
 int randomSeed;
 int maxSteps;
 
+#define FILE_READ_BUFFER_SIZE 4096
 #define ERROR_STR_BUFFER_SIZE 512
 
 void randrunInit(void);
 int parseArgs(int argc, char ** argv);
 void printUsageAndExit(char * progName, char * complain);
+
+int readFromFile(void);
+int generateRandomly(void);
 
 #endif /* RAND_RUN_H_ */
